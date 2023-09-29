080a17708859c2f1ec0861b82ca90ca8d704a7c8
Lael Medina <laelmedina90@gmail.com>
2023-09-29 10:18:18 -0600

    Add project files.

---
+++ Classes/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ReferenceForm.Classes
+{
+    class User
+    {
+        public int ReferenceId { get; set; }
+
+        public string Identification { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Gender { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string CreatedDate { get; set; } = DateTime.Now.ToShortDateString();
+
+        public string ModifiedDate { get; set; } = string.Empty;
+
+        public string DeletedDate { get; set; } = string.Empty;
+
+
+        public override string ToString()
+        {
+            return
+                $"Reference Id: {ReferenceId} \n" +
+                $"Identification: {Identification} \n" +
+                $"Name: {Name} \n" +
+                $"Last Name: {LastName} \n" +
+                $"Gender: {Gender} \n" +
+                $"Phone: {Phone} \n" +
+                $"Email: {Email} \n" +
+                $"Address: {Address} \n" +
+                $"Created Date: {CreatedDate} \n" +
+                $"Modified Date: {ModifiedDate} \n" +
+                $"Deleted Date: {DeletedDate} \n";
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using ReferenceForm.Classes;
+using System.Linq.Expressions;
+
+namespace ReferenceForm;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    User myUser = new();
+
+
+    private void btnLogIn_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            myUser.ReferenceId = int.Parse(txtIdReference.Text);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"{ex.Message}", "Warning");
+        }
+        
+        if (!string.IsNullOrEmpty(txtIdentification.Text) &&
+            !string.IsNullOrEmpty(txtName.Text) &&
+            !string.IsNullOrEmpty(txtLastName.Text) &&
+            !string.IsNullOrEmpty(txtEmail.Text) &&
+            !string.IsNullOrEmpty(txtAddress.Text) &&
+            !string.IsNullOrEmpty(txtPhone.Text) &&
+            !string.IsNullOrEmpty(txtModifiedDate.Text) &&
+            !string.IsNullOrEmpty(txtDeletedDate.Text))
+        {
+            myUser.Identification = txtIdentification.Text;
+
+            myUser.Name = txtName.Text;
+
+            myUser.LastName = txtLastName.Text;
+
+            myUser.Email = txtEmail.Text;
+
+            myUser.Address = txtAddress.Text;
+
+            myUser.Phone = txtPhone.Text;
+
+            myUser.ModifiedDate = txtModifiedDate.Text;
+
+            myUser.DeletedDate = txtDeletedDate.Text;
+
+            if (rbnMale.IsChecked == true)
+            {
+                myUser.Gender = "Male";
+            }
+            else
+            {
+                myUser.Gender = "Female";
+            }
+
+        }
+        else
+        {
+            MessageBox.Show("The field cannot be empty or null", "Invalid Input");
+        }
+
+        Serialize(myUser);
+
+        btnSeeInfo.IsEnabled = true;
+
+    }
+
+    private static void Serialize(User myUser)
+    {
+        string path = @"C:\Users\medin\OneDrive\Documentos\Coding\WPF\ReferenceForm\ReferenceForm\Files\FormInfo.txt";
+
+        if (File.Exists(path))
+        {
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(myUser.ToString());
+            }
+        }
+    }
+
+    private void BtnSeeInfo_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(
+                $"Reference Id: \t{myUser.ReferenceId} \n" +
+                $"Identification: \t{myUser.Identification} \n" +
+                $"Name: \t\t{myUser.Name} \n" +
+                $"Last Name: \t{myUser.LastName} \n" +
+                $"Gender: \t\t{myUser.Gender} \n" +
+                $"Phone: \t\t{myUser.Phone} \n" +
+                $"Email: \t\t{myUser.Email} \n" +
+                $"Address: \t\t{myUser.Address} \n" +
+                $"Created Date: \t{myUser.CreatedDate} \n" +
+                $"Modified Date: \t{myUser.ModifiedDate} \n" +
+                $"Deleted Date: \t{myUser.DeletedDate} \n"
+           );
+    }
+}
+
+
+//notes:
+//Study: Using statement
